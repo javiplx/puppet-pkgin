ffe4ca806ea54d43d72f1c2fafb69c8f383c1699
Javier Palacios <jpalacios@tcpsi.es>
2013-10-09 13:56:26 +0200

    Add docstrings and fix indentation

--- lib/puppet/provider/pkginrepo/parsed.rb
+++ lib/puppet/provider/pkginrepo/parsed.rb
@@ -5,7 +5,7 @@ repositories = "/opt/local/etc/pkgin/repositories.conf"
 
 Puppet::Type.type(:pkginrepo).provide(:parsed, :parent => Puppet::Provider::ParsedFile, :default_target => repositories , :filetype => :flat) do
 
-  desc "Provider"
+  desc "Basic provider for pkginrepo type"
 
   text_line :comment , :match => /^#/;
   text_line :blank , :match => /^\s*$/;
--- lib/puppet/type/pkginrepo.rb
+++ lib/puppet/type/pkginrepo.rb
@@ -1,11 +1,21 @@
 Puppet::Type.newtype(:pkginrepo) do
-  @doc = "Tipo para pkgin repositories"
+  @doc = "ParsedFile type to handle pkgin repositories"
 
   ensurable
 
-newparam(:url) do
-  desc "url for repository"
-  isnamevar
+  newparam(:url) do
+    desc "Repository url"
+    isnamevar
+  end
+
+  newproperty(:target) do
+    defaultto {
+      if @resource.class.defaultprovider.ancestors.include? (Puppet::Provider::ParsedFile)
+        @resouce.class.defaultprovider.default_target
+      else
+        nil
+      end
+    }
   end
 
 end
