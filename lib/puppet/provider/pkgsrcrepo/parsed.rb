d19bf52a69d78071d2e844d6c675e1020449db9a
javiplx <javiplx@gmail.com>
2013-10-09 07:31:55 -0700

    Merge pull request #8 from Telefonica/task/add_repo_type
    LGTM ...

---
+++ lib/puppet/provider/pkgsrcrepo/parsed.rb
@@ -0,0 +1,15 @@
+
+require 'puppet/provider/parsedfile'
+
+repositories = "/opt/local/etc/pkgin/repositories.conf"
+
+Puppet::Type.type(:pkgsrcrepo).provide(:parsed, :parent => Puppet::Provider::ParsedFile, :default_target => repositories , :filetype => :flat) do
+
+  desc "Basic provider for pkgsrcrepo type"
+
+  text_line :comment , :match => /^#/;
+  text_line :blank , :match => /^\s*$/;
+
+  record_line :parsed , :fields => %w{name}
+
+end
---
+++ lib/puppet/type/pkgsrcrepo.rb
@@ -0,0 +1,21 @@
+Puppet::Type.newtype(:pkgsrcrepo) do
+  @doc = "ParsedFile type to handle pkgsrc repositories"
+
+  ensurable
+
+  newparam(:url) do
+    desc "Repository url"
+    isnamevar
+  end
+
+  newproperty(:target) do
+    defaultto {
+      if @resource.class.defaultprovider.ancestors.include? (Puppet::Provider::ParsedFile)
+        @resouce.class.defaultprovider.default_target
+      else
+        nil
+      end
+    }
+  end
+
+end
