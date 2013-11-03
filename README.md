7bea349e62da15a39baac1c55efb1fc7e8dfbd9a
Javier Palacios <javiplx@gmail.com>
2013-11-03 11:06:04 +0000

    Remove development notes

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-## PDI aditions
-
-Besides some actual patching to pkgin module itself, there is a payload directory with some other modules. This is due to the solaris module dissapeared from puppetforge and to the fact that we have decided to create a single package with all the SmartOS related modules and this is the easiest way to to it.
-To actually build the package, you must clone on a directory named pkgin, and from the same level where this directory lives run
-
-    pkg_create \
-        -c pkgin/pkgbuild/summary -d pkgin/pkgbuild/description \
-        -B pkgin/pkgbuild/buildinfo -f pkgin/pkgbuild/packinlist \
-        -I /etc/puppet/modules is-puppet.tgz
-
-
 Puppet pkgin Module [![Build Status](https://secure.travis-ci.org/mudge/puppet-pkgin.png?branch=master)](http://travis-ci.org/mudge/puppet-pkgin)
 ===================
 
--- lib/puppet/provider/package/pkgin.rb
+++ lib/puppet/provider/package/pkgin.rb
@@ -24,27 +24,17 @@ Puppet::Type.type(:package).provide :pkgin, :parent => Puppet::Provider::Package
     end
   end
 
-  # packages : list of packages for the current provider declared on manifest
   def self.prefetch(packages)
-    # it is unclear if we should call the parent method here. The work done
-    #    there seems redundant, at least if pkgin is default provider.
     super
     pkgin("-y", :update)
   end
 
-  # called in every run to collect packages present in the system
-  # under 'apply', it is actually called from within the parent prefetch
   def self.instances
     pkgin(:list).split("\n").map do |package|
       new(parse(package))
     end
   end
 
-  # called for every resource in manifest not present in instances
-  # it is not defined wether this should query local or remote packages
-  # returned hash is stored on @property_hash, or '{:ensure=>:absent}' if nil
-  # or false is returnedm, but in any case install/update is executed depending
-  # on the value of the initial ensure attribute
   def query
     packages = parse_pkgsearch_line
 
